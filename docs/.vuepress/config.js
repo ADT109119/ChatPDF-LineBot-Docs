36e75813c432bbfafcdde7b3a3b83c1f21dcaa78
ADT109119 <2.jerry32262686@gmail.com>
2024-05-26 22:44:30 +0800

    Create config.js

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,31 @@
+import { viteBundler } from '@vuepress/bundler-vite'
+import { defaultTheme } from '@vuepress/theme-default'
+import { defineUserConfig } from 'vuepress'
+
+export default defineUserConfig({
+    title: 'ChatPDF-LineBot',
+    base: '/ChatPDF-LineBot-Docs/',
+  bundler: viteBundler(),
+  theme: defaultTheme({
+    repo: 'ADT109119/ChatPDF-LineBot',
+    docsRepo: 'ADT109119/ChatPDF-LineBot-Docs',
+    sidebar: {
+        '/': [
+          {
+            text: '文件',
+            children: [
+              {
+                text: '介紹',
+                link: '/',
+              },
+              '/installation.md',
+              '/configuration.md',
+              '/usage.md',
+              '/contact.md',
+            ],
+          },
+        ]
+  }
+  }),
+
+})
